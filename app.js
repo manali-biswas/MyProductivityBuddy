d47b74b5f7a19619da0cb69fc8658d0598116e0d
Manali Biswas <46257679+manali-biswas@users.noreply.github.com>
2020-10-14 17:30:38 +0530

    google calendar events

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const express=require('express');
+const app=express();
+
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+const port=process.env.PORT || 5000;
+
+
+app.get('/',function(req,res){    
+    res.render('home');
+});
+
+app.get('/google',function(req,res){
+    res.render('google');
+})
+
+app.listen(port,process.env.IP,function(){
+    console.log('Server is listening at '+port);
+});
